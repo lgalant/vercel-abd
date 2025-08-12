26d28c14aecc4689abf55cbd0be48a34b9cfae40
Leandro Galanterni <leandro.galanterni@ort.edu.ar>
2025-08-12 13:58:31 -0300

    db

--- dbconfig.js
+++ dbconfig.js
@@ -1,5 +1,5 @@
 export const config = {
-    host :process.env.PGHOST,
+    host : 'ep-odd-haze-adbv5w5c-pooler.c-2.us-east-1.aws.neon.tech',
     database:process.env.PGDATABASE,
     user:process.env.PGUSER,
     password:process.env.PGPASSWORD,
